713983dcff097e2fa81ca9d712e1fbd1cc59185e
Kyle Scumacher <kschumacher001@gmail.com>
2020-10-26 12:56:12 -0400

    completed

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace OperatorExercise
 {
@@ -6,7 +7,40 @@ namespace OperatorExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            Console.WriteLine($"{a}*{b} is {product}");
+            Console.WriteLine($"{a}+{b} is {sum}");
+            Console.WriteLine($"{a}-{b} is {difference}");
+            Console.WriteLine("Press enter to continue.");
+
+            Console.ReadLine();
+            Console.Clear();
+            
+            Console.WriteLine("Enter radius.");
+
+            double pi = 3.14159;
+            double r = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(pi, r);
+
+            Console.Clear();
+
+            Console.WriteLine($"The area of your cirlce is {area} square feet.");
         }
+
+        public static double AreaOfCircle(double pi, double r)
+        {
+            return pi * (r * r);
+        }
+
+
     }
 }
